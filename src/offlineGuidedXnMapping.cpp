93ce899550d252b5cef9376c69a8a8ca91e284d5
WT411-002LCW <mhossain@Cogbot>
2014-12-19 14:25:05 +1300

    renamed:    src/OfflineGuidedXnMapping.cpp -> src/offlineGuidedXnMapping.cpp

--- src/offlineGuidedXnMapping.cpp
+++ src/offlineGuidedXnMapping.cpp
@@ -14,6 +14,7 @@
 #include <sstream>
 #include <cmath>
 #include <math.h>
+#include <string>
 
 /* ------------------------- Robot includes ------------------------- */
 #include "Aria.h"
@@ -32,14 +33,15 @@
 /* ------------------------- Open CV includes ------------------------- */
 #include <opencv/cv.h>
 #include <opencv2/opencv.hpp>
+#include <boost/filesystem/operations.hpp>
 //#include <mrpt/bayes/CParticleFilterCapable.h>
 
 
 /* -------------------------Segmentation includes ------------------------- */
-#include "../include/segmentation/image.h"
-#include "../include/segmentation/misc.h"
-#include "../include/segmentation/pnmfile.h"
-#include "../include/segmentation/segment-image.h"
+//#include "../include/segmentation/image.h"
+//#include "../include/segmentation/misc.h"
+//#include "../include/segmentation/pnmfile.h"
+//#include "../include/segmentation/segment-image.h"
 
 /* ------------------------- Headers ------------------------ */
 #include "Camera.h"
@@ -64,29 +66,15 @@ int main(int argc, char** argv) {
     /*------------------------------------------ Variables declaration ------------------------------------------ */
 
     Robot Albot;
-    ArSimpleConnector connector(&argc, argv);
 
     Camera Bumblebee;
     View curView;
+    curView.setRobotSurfaces(Albot.getRectRobot());
     Map curMap(1500, 1500);
     Map localSpace(1500, 1500);
-    PathFinder pathFinder;
-    AngleAndDistance nextGoal;
 
-    Printer printer;
 
-    Albot.saveTravelInfo(0, 0, "../outputs/surfaces/coordTrans-0");
-
-    SameSurfaceFinderOdo sameSurfaceFinderOdo;
-
-    /*------------------------------------------ Construction & Initialization ------------------------------------------ */
-
- //   Albot.connect(argc, argv, &connector);
-
-   // Bumblebee.initialize();
-    Bumblebee.setV(0);
-
-    /*------------------------------------------ Start Xploring ------------------------------------------ */
+/*------------------------------------------ Start Xploring ------------------------------------------ */
 
     //curView.printView();
     char viewName[50], mapName[50];
@@ -94,62 +82,126 @@ int main(int argc, char** argv) {
     bool initializeLocalSpace = true;
     int localSpaceCounter = 0;
 
-    
-    
     /* -------- Loop ------- */
-    char tkStep = 'y';
+    char tkStep = 'y';    
+    bool GLOBAL_MAP = false;
+    cout << endl << endl << "Compute Global Map? (y/n) "; // Ask user if continue
+    cin >> tkStep;
+    if (tkStep != 'n' && tkStep != 'N')
+        GLOBAL_MAP = true;
+
+    tkStep = 'y';
+
 
     while (tkStep != 'n' && tkStep != 'N') {
         /* Increment counters */
         //read view.
         curView.setId(curView.getId() + 1);
-        sprintf(viewName, "%s%d", "../outputs/localSpaces/localSpace-", curView.getId());
-        readAView(curView,viewName );
+        sprintf(viewName, "%s%d", "../outputs/surfaces/surfaces-", curView.getId());
+        readAView(curView, viewName);
         curView.setRobotSurfaces(Albot.getRectRobot());
-
         curView.markLandmarks();
-        
-        sprintf(viewName, "%s%d%s", "../outputs/Views/View-", curView.getId(), ".png");
+
+        sprintf(viewName, "%s%d%s", "../outputs/Maps/View-", curView.getId(), ".png");
         plotViewGNU(viewName, curView);
-        
+
         cout << endl << "==================================================" << endl << endl;
-        cout << BOLDRED << "                View no. " << curView.getId() << ":" << RESET << endl;
+        cout << "View no. " << curView.getId() << ":" << endl;
         plotViewGNU("../outputs/Maps/currentView.png", curView);
+        cout << endl << endl << "Initialize local space? (y/n) "; // Ask user if continue
+        cin >> tkStep;
+
+        if (tkStep == 'y' or tkStep == 'Y')
+            initializeLocalSpace = true;
+
+        if (initializeLocalSpace == true) {
+            //save localSpace in a txt file before starting new.
+            if (localSpaceCounter != 0) {
+                if (EGOCENTRIC_REFERENCE_FRAME == true)
+                    localSpace.addPVUsingOdo(curView, Albot.getLocalSpaceHome());
+                else
+                    localSpace.addCVUsingOdo(curView, Albot.getLocalSpaceHome());
+                sprintf(mapName, "%s%d%s%d%s", "../outputs/Maps/LS-", localSpaceCounter, "-v-", curView.getId(), "a-before.png");
+                plotMapGNU(mapName, localSpace);
+
+                localSpace.cleanMapUsingOdo(curView, Albot.getLocalSpaceHome());
+                sprintf(mapName, "%s%d%s%d%s", "../outputs/Maps/LS-", localSpaceCounter, "-v-", curView.getId(), "b-after.png");
+                plotMapGNU(mapName, localSpace);
 
 
-        if (COMPUTE_GLOBAL_MAP == true) {
-            
+                sprintf(mapName, "%s%d", "../outputs/localSpaces/localSpace-", localSpaceCounter);
+                localSpace.saveInTxtFile(mapName, localSpace.transformToGlobalMap(curView.getRobotSurfaces(), localSpace.getPathSegments()));
+            }
+            localSpaceCounter++;
+            cout << BOLDMAGENTA << "Started local space >> " << BOLDRED << localSpaceCounter << RESET << endl;
+            localSpace.initializeMap(curView);
+
+
+            Albot.setLocalSpaceHome(0, 0);
+            sprintf(mapName, "%s%d%s%d%s", "../outputs/Maps/LS-", localSpaceCounter, "-v-", curView.getId(), ".png");
+            plotMapGNU(mapName, localSpace);
+            initializeLocalSpace = false;
+        } else {//update local space
+            cout << BOLDMAGENTA << "Expended local space >> " << BOLDRED << localSpaceCounter << RESET << endl;
+            if (EGOCENTRIC_REFERENCE_FRAME == true)
+                localSpace.addPVUsingOdo(curView, Albot.getLocalSpaceHome());
+            else
+                localSpace.addCVUsingOdo(curView, Albot.getLocalSpaceHome());
+            sprintf(mapName, "%s%d%s%d%s", "../outputs/Maps/LS-", localSpaceCounter, "-v-", curView.getId(), "a-before.png");
+            plotMapGNU(mapName, localSpace);
+
+            //            cout<<"surfaces after adding.."<<endl;
+            //            for(unsigned int i=0; i<localSpace.getMap().size(); i++)
+            //                cout<<i+1<<" surfs: "<<localSpace.getMap()[i].getSurfaces().size()<<endl;
+
+            localSpace.cleanMapUsingOdo(curView, Albot.getLocalSpaceHome());
+            sprintf(mapName, "%s%d%s%d%s", "../outputs/Maps/LS-", localSpaceCounter, "-v-", curView.getId(), "b-after.png");
+            plotMapGNU(mapName, localSpace);
+
+            //            cout<<"surfaces after cleaning.."<<endl;
+            //            for(unsigned int i=0; i<localSpace.getMap().size(); i++)
+            //                cout<<i+1<<" surfs: "<<localSpace.getMap()[i].getSurfaces().size()<<endl;
+        }
+
+        if (GLOBAL_MAP == true) {
+            cout << BOLDCYAN << "Updating map >> " << BOLDRED << RESET << endl;
             if (curView.getId() == 1) {//initialize once.
                 curMap.initializeMap(curView);
                 sprintf(mapName, "%s%d%s", "../outputs/Maps/Map-v-", curView.getId(), ".png");
                 plotMapGNU(mapName, curMap);
-                cout << BOLDGREEN << "Map has been initialized...>> " << BOLDRED << curView.getId() << RESET << endl;
             } else {
-                cout << BOLDCYAN << "Updating map >> @" << BOLDRED << curView.getId() << RESET << endl;
                 //update map.
-                curMap.expandMap(curView);
+                curMap.addCVUsingOdo(curView, Albot.getFromHome());
 
                 if (PRINT_GLOBAL_MAP) {
                     sprintf(mapName, "%s%d%s", "../outputs/Maps/Map-v-", curView.getId(), "a-before.png");
                     plotMapGNU(mapName, curMap);
                 }
 
-//              //  curMap.cleanMapUsingOdo(curView, Albot.getFromHome());
-//                if (PRINT_GLOBAL_MAP) {
-//                    sprintf(mapName, "%s%d%s", "../outputs/Maps/Map-v-", curView.getId(), "b-after.png");
-//                    plotMapGNU(mapName, curMap);
-//                }
+                curMap.cleanMapUsingOdo(curView, Albot.getFromHome());
+                if (PRINT_GLOBAL_MAP) {
+                    sprintf(mapName, "%s%d%s", "../outputs/Maps/Map-v-", curView.getId(), "b-after.png");
+                    plotMapGNU(mapName, curMap);
+                }
             }
         }
-
         cout << endl << endl << "Take another step? (y/n) "; // Ask user if continue
         cin >> tkStep;
 
-        //save cView as pView for next step.
-        curMap.setPreviousView(curView);
+        /* Move Albot using user input */
+        if (tkStep != 'n' && tkStep != 'N') {
+            //read odometer info
+            sprintf(viewName, "%s%d", "../outputs/surfaces/coordTrans-", curView.getId());
+            readOdometry(Albot, viewName);
+            localSpace.addPathSegment(Albot.getLastLocomotion());
+            curMap.addPathSegment(Albot.getLastLocomotion());
+        }
     }
-
-    if (COMPUTE_GLOBAL_MAP == true)
+    if (localSpaceCounter != 0) {//for last local space
+        sprintf(mapName, "%s%d", "../outputs/localSpaces/localSpace-", localSpaceCounter);
+        localSpace.saveInTxtFile(mapName, localSpace.transformToGlobalMap(curView.getRobotSurfaces(), localSpace.getPathSegments()));
+    }
+    if (GLOBAL_MAP == true)
         plotMapGNU("../outputs/Maps/Global-Map.png", curMap);
     cout << endl;
 
